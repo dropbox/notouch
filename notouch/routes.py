e40346b0ff8dede225c5f621c8a5b392f8f98009
Matt Cote <mcot@dropbox.com>
2015-12-08 17:49:57 -0800

    Fix tabs to spaces in routes.py.

--- notouch/routes.py
+++ notouch/routes.py
@@ -2,9 +2,9 @@ from .handlers import api
 from .handlers import main
 
 HANDLERS = [
-	(r"/", main.MainHandler),
-	
+    (r"/", main.MainHandler),
+
     # v1 API Handlers
-	(r"/api/v1/dhcp/ack", api.DHCPAckApiV1Handler),
+    (r"/api/v1/dhcp/ack", api.DHCPAckApiV1Handler),
     (r"/api/v1/dhcp/server_stats", api.DHCPServerStatsApiV1Handler),
 ]
